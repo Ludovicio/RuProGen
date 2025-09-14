b680d5bf0af41a2bd0450facc2763b84ab7276ee
Israel Bautista <isra.bautista@gmail.com>
2025-09-14 16:55:21 +0200

    Added controls por perlins parameters.

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use iced::{
     widget::{
         button,
         canvas::{self, Canvas, Event, Frame, Geometry, Path, Program, Image as CanvasImage},
-        column, container,
+        row, column, slider, container, text, horizontal_rule,
     },
     widget::image::Handle,
     Color, Element, Length, Point, Rectangle,
@@ -35,15 +35,38 @@ fn main() -> iced::Result {
 
 #[derive(Default)]
 struct PaintApp {
+    // TODO: hay que eliminar points de toda la app
     points: Vec<Point>,
     image: Option<(u32, u32, Vec<u8>)>, // ancho, alto, pixels RGBA
+    octaves: u32,
+    lacunarity: u32,
+    persistence: u32,
+    imgWidth: u32,
+    imgHeight: u32,
 }
+
+
+// TODO: Implementar Default para PaintApp esto es solo un ejemplo
+// impl Default for struct PaintApp {
+//     fn default() -> Self {
+//         Config {
+//             width: 1920,
+//             height: 1080,
+//             fullscreen: false,
+//         }
+//     }
+// }
 
 #[derive(Debug, Clone)]
 enum Message {
     Clicked(Point),
     Clear,
     ApplyTestImage,
+    OctavesChanged(u32),
+    LacunarityChanged(u32),
+    PersistenceChanged(u32),
+    ImgWidthChanged(u32),
+    ImgHeightChanged(u32),
 }
 
 // TODO: Sliders para parámetros.
@@ -59,23 +82,31 @@ impl PaintApp {
             Message::ApplyTestImage => {
                 // Creamos una imagen de prueba: 10x20 con patrón simple
                 let randnum = rand::thread_rng().gen_range(0..100);
-                let width = 300;
-                let height = 300;
-                let mut pixels = Vec::with_capacity((width * height * 4) as usize);
+                // let width = 300;
+                // let height = 300;
+                let mut pixels = Vec::with_capacity((self.imgWidth * self.imgHeight * 4) as usize);
                 let perlin = Perlin::new(randnum);
+                let dlacunarity: f64  = (self.lacunarity as f64) / 10.0;
+                let dpersistence: f64 = (self.persistence as f64) / 100.0;
 
-                for j in 0..(height) {
-                  for i in 0..(width) {
-                    let x = i as f64 / width as f64;
-                    let y = j as f64 / height as f64;
-                    let prev = fractal_noise(&perlin, [x, y], 8, 6.0, 0.9);
+                for j in 0..(self.imgHeight) {
+                  for i in 0..(self.imgWidth) {
+                    let x = i as f64 / self.imgWidth as f64;
+                    let y = j as f64 / self.imgHeight as f64;
+                    // let prev = fractal_noise(&perlin, [x, y], 8, 6.0, 0.9);
+                    let prev = fractal_noise(&perlin, [x, y], self.octaves, dlacunarity, dpersistence);
                     let value: u8 = (prev * 255.999) as u8;
                     pixels.extend_from_slice(&[value, value, value, 255]);
                   }
                 }
 
-                self.image = Some((width, height, pixels));
-            }
+                self.image = Some((self.imgWidth, self.imgHeight, pixels));
+            },
+            Message::OctavesChanged(val) => self.octaves = val,
+            Message::LacunarityChanged(val) => self.lacunarity = val,
+            Message::PersistenceChanged(val) => self.persistence = val,
+            Message::ImgWidthChanged(val) => self.imgWidth = val,
+            Message::ImgHeightChanged(val) => self.imgHeight = val,
         }
     }
 
@@ -86,11 +117,72 @@ impl PaintApp {
         })
         .width(Length::Fill)
         .height(Length::Fill);
+        let octavesSlider = container(
+            slider(1..=50, self.octaves, Message::OctavesChanged)
+                .default(8u32)
+                .shift_step(1u32),
+        )
+        .width(250);
+        let octavesSliderText = text(format!("Octavas: {}", self.octaves));
 
-        let content = column![
+        let lacunaritySlider = container(
+            slider(1..=40, self.lacunarity, Message::LacunarityChanged)
+                .default(8u32)
+                .shift_step(1u32),
+        )
+        .width(250);
+        let lacunaritySliderText = text(format!("lacunaridad: {}", (self.lacunarity as f64) / 10.0));
+
+        let persistenceSlider = container(
+            slider(1..=100, self.persistence, Message::PersistenceChanged)
+                .default(8u32)
+                .shift_step(1u32),
+        )
+        .width(250);
+        let persistenceSliderText = text(format!("Persistencia: {}", (self.persistence as f64) / 100.0));
+
+        let imgWidthSlider = container(
+            slider(50..=2000, self.imgWidth, Message::ImgWidthChanged)
+                .default(800u32)
+                .shift_step(100u32),
+        )
+        .width(250);
+        let imgWidthSliderText = text(format!("Ancho imagen: {}", self.imgWidth));
+
+        let imgHeightSlider = container(
+            slider(50..=2000, self.imgHeight, Message::ImgHeightChanged)
+                .default(600u32)
+                .shift_step(100u32),
+        )
+        .width(250);
+        let imgHeightSliderText = text(format!("Alto imagen: {}", self.imgHeight));
+
+        let controls = column![
             button("Limpiar").on_press(Message::Clear),
             button("Aplicar imagen de prueba").on_press(Message::ApplyTestImage),
+            octavesSliderText, octavesSlider,
+            horizontal_rule(1),
+            lacunaritySliderText, lacunaritySlider,
+            horizontal_rule(1),
+            persistenceSliderText, persistenceSlider,
+            horizontal_rule(1),
+            imgWidthSliderText, imgWidthSlider,
+            horizontal_rule(1),
+            imgHeightSliderText, imgHeightSlider,
+        ]
+        .padding(12)
+        .spacing(12);
+        //.width(Length::Shrink);
+
+        let viewer = column![
             canvas,
+        ]
+        .padding(12)
+        .spacing(12);
+
+        let content = row![
+            controls,
+            viewer,
         ]
         .padding(12)
         .spacing(12);
@@ -104,6 +196,7 @@ impl PaintApp {
 
 #[derive(Clone)]
 struct DotsProgram {
+    // TODO: hay que eliminar points de toda la app
     points: Vec<Point>,
     image: Option<(u32, u32, Vec<u8>)>,
 }
@@ -137,6 +230,7 @@ impl Program<Message> for DotsProgram {
         bounds: Rectangle,
         _cursor: mouse::Cursor,
     ) -> Vec<Geometry> {
+        // TODO: Ya no vamos a pintar puntos, solo la imagen
         let mut frame = Frame::new(renderer, bounds.size());
 
         // Dibujar imagen si existe
