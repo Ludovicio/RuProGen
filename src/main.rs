420e2b3b14610b97702f8bc7494490282fc25b39
Israel Bautista <isra.bautista@gmail.com>
2025-10-26 18:10:55 +0100

    Limpiado código de puntos. Refactorizados nombres para quitar warnings.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ use iced::{
     mouse,
     widget::{
         button,
-        canvas::{self, Canvas, Event, Frame, Geometry, Path, Program, Image as CanvasImage},
+        canvas::{self, Canvas, Event, Frame, Geometry, Program, Image as CanvasImage},
         row, column, slider, container, text, horizontal_rule,
     },
     widget::image::Handle, widget::scrollable::Scrollbar, widget::scrollable::Direction,
-    Color, Element, Length, Point, Rectangle,
+    Element, Length, Rectangle,
 };
 
 use noise::{NoiseFn, Perlin};
@@ -40,44 +40,40 @@ fn main() -> iced::Result {
 }
 
 struct PaintApp {
-    // TODO: hay que eliminar points de toda la app
-    points: Vec<Point>,
     image: Option<(u32, u32, Vec<u8>)>, // ancho, alto, pixels RGBA
     octaves: u32,
     lacunarity: u32,
     persistence: u32,
     frequency: u32,
     amplitude: u32,
-    imgWidth: u32,
-    imgHeight: u32,
+    img_width: u32,
+    img_height: u32,
 }
 
 impl Default for PaintApp {
     fn default() -> Self {
         PaintApp {
-            points: Vec::new(),
             image: None,
             octaves: 8,
             lacunarity: 20,
             persistence: 50,
             frequency: 50,
             amplitude: 50,
-            imgWidth: 800,
-            imgHeight: 600,
+            img_width: 800,
+            img_height: 600,
         }
     }
 }
 
 #[derive(Debug, Clone)]
 enum Message {
-    Clicked(Point),
     Clear,
     ApplyTestImage,
     OctavesChanged(u32),
     LacunarityChanged(u32),
     PersistenceChanged(u32),
-    dAmplitudeChanged(u32),
-    dFrequencyChanged(u32),
+    DAmplitudeChanged(u32),
+    DFrequencyChanged(u32),
     ImgWidthChanged(u32),
     ImgHeightChanged(u32),
 }
@@ -85,133 +81,129 @@ enum Message {
 impl PaintApp {
     fn update(&mut self, message: Message) {
         match message {
-            Message::Clicked(pos) => self.points.push(pos),
             Message::Clear => {
-              self.points.clear();
               self.image = None;
             },
             Message::ApplyTestImage => {
                 let randnum = rand::thread_rng().gen();
-                let mut pixels = Vec::with_capacity((self.imgWidth * self.imgHeight * 4) as usize);
+                let mut pixels = Vec::with_capacity((self.img_width * self.img_height * 4) as usize);
                 let perlin = Perlin::new(randnum);
                 let dlacunarity:  f64 = (self.lacunarity as f64) / 10.0;
                 let dpersistence: f64 = (self.persistence as f64) / 100.0;
-                let dAmplitude:   f64 = (self.amplitude as f64) / 100.0;
-                let dFrequency:   f64 = (self.frequency as f64) / 100.0;
+                let d_amplitude:   f64 = (self.amplitude as f64) / 100.0;
+                let d_frequency:   f64 = (self.frequency as f64) / 100.0;
 
-                for j in 0..(self.imgHeight) {
-                  for i in 0..(self.imgWidth) {
-                    let x = i as f64 / self.imgWidth as f64;
-                    let y = j as f64 / self.imgHeight as f64;
-                    let prev = fractal_noise(&perlin, [x, y], self.octaves, dlacunarity, dpersistence, dFrequency, dAmplitude);
+                for j in 0..(self.img_height) {
+                  for i in 0..(self.img_width) {
+                    let x = i as f64 / self.img_width as f64;
+                    let y = j as f64 / self.img_height as f64;
+                    let prev = fractal_noise(&perlin, [x, y], self.octaves, dlacunarity, dpersistence, d_frequency, d_amplitude);
                     pixels.extend_from_slice(&perlin_to_color(prev));
                     //let value: u8 = (prev * 255.999) as u8;
                     //pixels.extend_from_slice(&[value, value, value, 255]);
                   }
                 }
 
-                self.image = Some((self.imgWidth, self.imgHeight, pixels));
+                self.image = Some((self.img_width, self.img_height, pixels));
             },
             Message::OctavesChanged(val) => self.octaves = val,
             Message::LacunarityChanged(val) => self.lacunarity = val,
             Message::PersistenceChanged(val) => self.persistence = val,
-            Message::dAmplitudeChanged(val) => self.amplitude = val,
-            Message::dFrequencyChanged(val) => self.frequency = val,
-            Message::ImgWidthChanged(val) => self.imgWidth = val,
-            Message::ImgHeightChanged(val) => self.imgHeight = val,
+            Message::DAmplitudeChanged(val) => self.amplitude = val,
+            Message::DFrequencyChanged(val) => self.frequency = val,
+            Message::ImgWidthChanged(val) => self.img_width = val,
+            Message::ImgHeightChanged(val) => self.img_height = val,
         }
     }
 
     fn view(&self) -> Element<Message> {
         use iced::widget::scrollable;
         let canvas = Canvas::new(DotsProgram {
-            points: self.points.clone(),
             image: self.image.clone(),
         })
-        .width(Length::Fixed(self.imgWidth as f32))
-        .height(Length::Fixed(self.imgHeight as f32));
+        .width(Length::Fixed(self.img_width as f32))
+        .height(Length::Fixed(self.img_height as f32));
 
         let scrollable_canvas = scrollable(canvas)
             .width(Length::Fill)
             .height(Length::Fill)
             .direction(Direction::Both { vertical: Scrollbar::new(), horizontal: Scrollbar::new() });
             
-        let octavesSlider = container(
-            slider(1..=50, self.octaves, Message::OctavesChanged)
+        let octaves_slider = container(
+            slider(1..=20, self.octaves, Message::OctavesChanged)
                 .default(8u32)
                 .shift_step(1u32),
         )
         .width(250);
-        let octavesSliderText = text(format!("Octavas: {}", self.octaves));
+        let octaves_slider_text = text(format!("Octavas: {}", self.octaves));
 
-        let lacunaritySlider = container(
+        let lacunarity_slider = container(
             slider(1..=40, self.lacunarity, Message::LacunarityChanged)
                 .default(8u32)
                 .shift_step(1u32),
         )
         .width(250);
-        let lacunaritySliderText = text(format!("lacunaridad: {}", (self.lacunarity as f64) / 10.0));
+        let lacunarity_slider_text = text(format!("lacunaridad: {}", (self.lacunarity as f64) / 10.0));
 
-        let persistenceSlider = container(
+        let persistence_slider = container(
             slider(1..=100, self.persistence, Message::PersistenceChanged)
                 .default(8u32)
                 .shift_step(1u32),
         )
         .width(250);
-        let persistenceSliderText = text(format!("Persistencia: {}", (self.persistence as f64) / 100.0));
+        let persistence_slider_text = text(format!("Persistencia: {}", (self.persistence as f64) / 100.0));
 
-        let dAmplitudeSlider = container(
-            slider(1..=1000, self.amplitude, Message::dAmplitudeChanged)
+        let d_amplitude_slider = container(
+            slider(1..=1000, self.amplitude, Message::DAmplitudeChanged)
                 .default(50u32)
                 .shift_step(1u32),
         )
         .width(250);
-        let dAmplitudeSliderText = text(format!("Amplitud: {}", (self.amplitude as f64) / 100.0));
+        let d_amplitude_slider_text = text(format!("Amplitud: {}", (self.amplitude as f64) / 100.0));
 
-        let dFrequencySlider = container(
-            slider(1..=1000, self.frequency, Message::dFrequencyChanged)
+        let d_frequency_slider = container(
+            slider(1..=10000, self.frequency, Message::DFrequencyChanged)
                 .default(50u32)
                 .shift_step(1u32),
         )
         .width(250);
-        let dFrequencySliderText = text(format!("Frecuencia: {}", (self.frequency as f64) / 100.0));
+        let d_frequency_slider_text = text(format!("Frecuencia: {}", (self.frequency as f64) / 100.0));
 
-        let imgWidthSlider = container(
-            slider(50..=2000, self.imgWidth, Message::ImgWidthChanged)
+        let img_width_slider = container(
+            slider(50..=2000, self.img_width, Message::ImgWidthChanged)
                 .default(800u32)
                 .shift_step(100u32),
         )
         .width(250);
-        let imgWidthSliderText = text(format!("Ancho imagen: {}", self.imgWidth));
+        let img_width_slider_text = text(format!("Ancho imagen: {}", self.img_width));
 
-        let imgHeightSlider = container(
-            slider(50..=2000, self.imgHeight, Message::ImgHeightChanged)
+        let img_height_slider = container(
+            slider(50..=2000, self.img_height, Message::ImgHeightChanged)
                 .default(600u32)
                 .shift_step(100u32),
         )
         .width(250);
-        let imgHeightSliderText = text(format!("Alto imagen: {}", self.imgHeight));
+        let img_height_slider_text = text(format!("Alto imagen: {}", self.img_height));
 
         let controls = column![
             button("Limpiar").on_press(Message::Clear),
             button("Aplicar imagen de prueba").on_press(Message::ApplyTestImage),
-            octavesSliderText, octavesSlider,
+            octaves_slider_text, octaves_slider,
             horizontal_rule(1),
-            lacunaritySliderText, lacunaritySlider,
+            lacunarity_slider_text, lacunarity_slider,
             horizontal_rule(1),
-            persistenceSliderText, persistenceSlider,
+            persistence_slider_text, persistence_slider,
             horizontal_rule(1),
-            dAmplitudeSliderText, dAmplitudeSlider,
+            d_amplitude_slider_text, d_amplitude_slider,
             horizontal_rule(1),
-            dFrequencySliderText, dFrequencySlider,
+            d_frequency_slider_text, d_frequency_slider,
             horizontal_rule(1),
-            imgWidthSliderText, imgWidthSlider,
+            img_width_slider_text, img_width_slider,
             horizontal_rule(1),
-            imgHeightSliderText, imgHeightSlider,
+            img_height_slider_text, img_height_slider,
         ]
         .padding(12)
         .spacing(12)
-        //.width(Length::Fixed(150));
         .width(Length::Shrink);
 
         let viewer = column![
@@ -236,8 +228,6 @@ impl PaintApp {
 
 #[derive(Clone)]
 struct DotsProgram {
-    // TODO: hay que eliminar points de toda la app
-    points: Vec<Point>,
     image: Option<(u32, u32, Vec<u8>)>,
 }
 
@@ -247,18 +237,10 @@ impl Program<Message> for DotsProgram {
     fn update(
         &self,
         _state: &mut Self::State,
-        event: Event,
-        bounds: Rectangle,
-        cursor: mouse::Cursor,
+        _event: Event,
+        _bounds: Rectangle,
+        _cursor: mouse::Cursor,
     ) -> (canvas::event::Status, Option<Message>) {
-        if let Event::Mouse(mouse::Event::ButtonPressed(mouse::Button::Left)) = event {
-            if let Some(pos) = cursor.position_in(bounds) {
-                return (
-                    canvas::event::Status::Captured,
-                    Some(Message::Clicked(pos)),
-                );
-            }
-        }
         (canvas::event::Status::Ignored, None)
     }
 
@@ -270,24 +252,16 @@ impl Program<Message> for DotsProgram {
         bounds: Rectangle,
         _cursor: mouse::Cursor,
     ) -> Vec<Geometry> {
-        // TODO: Ya no vamos a pintar puntos, solo la imagen
         let mut frame = Frame::new(renderer, bounds.size());
-
         // Dibujar imagen si existe
         if let Some((width, height, pixels)) = &self.image {
             self.draw_image_from_rgba(&mut frame, *width, *height, pixels);
         }
 
-        // Dibujar puntos clicados
-        for &p in &self.points {
-            let dot = Path::circle(p, 4.0);
-            frame.fill(&dot, Color::from_rgb(0.2, 0.4, 0.9));
-        }
-
         vec![frame.into_geometry()]
     }
 }
-// TODO: Revisar máximo octavas.
+
 impl DotsProgram {
     fn draw_image_from_rgba(
         &self,
@@ -313,3 +287,6 @@ impl DotsProgram {
         frame.draw_image(bounds, canvas_img);
     }
 }
+
+// TODO revisar, entender y reescribit pintado.
+// Centralizar valores minimos y máximos de sliders y demás controles
